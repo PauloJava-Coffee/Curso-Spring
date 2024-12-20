2238131558895e2364865f8e820d2ef84216dcfb
PauloJava-Coffee <paulotricardo@outlook.com>
2024-12-20 15:28:17 -0300

    Mapeamento das entidades autor e livro

---
+++ Exercicios/libraryapi/src/main/java/io/github/paulojava_coffee/libraryapi/model/Autor.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package io.github.paulojava_coffee.libraryapi.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author barbo
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@Entity
+@Table(name = "autor", schema = "public")
+public class Autor {
+    
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    
+    @Column(name = "nome", length = 100, nullable = false)
+    private String nome;
+    
+    @Column(name = "data_nascimento" , nullable = false)
+    private LocalDate dataNascimento;
+    
+    @Column(name = "nacionalidade", length = 50, nullable = false)
+    private String nacionalidade;
+    
+    @OneToMany(mappedBy = "autor")
+    private List<Livro> livros;
+    
+    @Deprecated
+    public Autor(){
+       //PARA USO DO FRAMEWORK 
+    }
+    
+    
+    
+    
+
+}
---
+++ Exercicios/libraryapi/src/main/java/io/github/paulojava_coffee/libraryapi/model/Livro.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package io.github.paulojava_coffee.libraryapi.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
+import java.util.UUID;
+import lombok.Data;
+
+
+/**
+ *
+ * @author barbo
+ */
+@Data
+@Entity
+public class Livro {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "isbn", length = 20, nullable = false)
+    private String isbn;
+
+    @Column(name = "data_publicacao")
+    private LocalDate dataPublicacao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "genero", length = 30, nullable = false)
+    private GeneroLivro genero;
+
+    @Column(name = "preco", precision = 18, scale = 2)
+    private Double preco;
+
+    @ManyToOne
+    @JoinColumn
+    private Autor autor;
+}
