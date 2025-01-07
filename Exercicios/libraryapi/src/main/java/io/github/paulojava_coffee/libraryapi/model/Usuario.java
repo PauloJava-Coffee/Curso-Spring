e1185468634681084672a52b5fbc120b57aa073b
PauloJava-Coffee <paulotricardo@outlook.com>
2025-01-07 07:18:47 -0300

    Mapeamento da tabela de usuários

---
+++ Exercicios/libraryapi/src/main/java/io/github/paulojava_coffee/libraryapi/model/Usuario.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package io.github.paulojava_coffee.libraryapi.model;
+
+import io.hypersistence.utils.hibernate.type.array.ListArrayType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.List;
+import java.util.UUID;
+import lombok.Data;
+import org.hibernate.annotations.Type;
+
+/**
+ *
+ * @author santa
+ */
+@Entity
+@Table
+@Data
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String login;
+
+    @Column(nullable = false, length = 300)
+    private String senha;
+
+    @Type(ListArrayType.class)
+    @Column(name = "roles", columnDefinition = "varchar[]")
+    private List<String> roles;
+}
