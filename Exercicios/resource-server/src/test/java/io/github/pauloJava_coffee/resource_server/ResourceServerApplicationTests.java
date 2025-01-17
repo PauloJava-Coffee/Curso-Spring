da8c4b0d5387525250d34a0964562363173c1f83
PauloJava-Coffee <paulotricardo@outlook.com>
2025-01-17 05:25:26 -0300

    Criação do projeto "Resource-Server"

---
+++ Exercicios/resource-server/src/test/java/io/github/pauloJava_coffee/resource_server/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pauloJava_coffee.resource_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
